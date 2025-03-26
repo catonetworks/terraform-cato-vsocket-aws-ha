7c54095cc5e679211be6935f64a552f3ddbdd808
Slowtyping-cmd <85334596+Slowtyping-cmd@users.noreply.github.com>
2025-03-26 17:33:07 +0000

    First commit of AWS HA Module

---
+++ main.tf
@@ -0,0 +1,342 @@
+provider "aws" {
+  region = var.region
+}
+
+provider "cato" {
+  baseurl    = var.baseurl
+  token      = var.token
+  account_id = var.account_id
+}
+
+resource "cato_socket_site" "aws-site" {
+  connection_type = var.connection_type
+  description     = var.site_description
+  name            = var.site_name
+  native_range = {
+    native_network_range = var.native_network_range
+    local_ip             = var.lan_local_ip
+  }
+  site_location = var.site_location
+  site_type     = var.site_type
+}
+
+data "cato_accountSnapshotSite" "aws-site" {
+  id = cato_socket_site.aws-site.id
+}
+
+# AWS HA IAM role configuration
+resource "aws_iam_role" "cato_ha_role" {
+  name        = "Rgood-Cato-HA-Role"
+  description = "To allow EC2 route management"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow"
+      Action = "sts:AssumeRole"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+
+resource "aws_iam_policy" "cato_ha_policy" {
+  name = "Rgood-Cato-HA-Role-Policy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateRoute",
+          "ec2:DescribeRouteTables",
+          "ec2:ReplaceRoute"
+        ]
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "cato_ha_attach" {
+  role       = aws_iam_role.cato_ha_role.name
+  policy_arn = aws_iam_policy.cato_ha_policy.arn
+}
+
+resource "aws_iam_instance_profile" "cato_ha_instance_profile" {
+  name = "Rgood-Cato-HA-Role"
+  role = aws_iam_role.cato_ha_role.name
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = var.vpc_id
+  tags = {
+    Name = "rgood_ig"
+  }
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+}
+
+
+## Lookup data from region and VPC
+data "aws_ami" "vsocket" {
+  most_recent = true
+  name_regex  = "VSOCKET_AWS"
+  owners      = ["aws-marketplace"]
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Create Primary vSocket Virtual Machine
+resource "aws_instance" "primary_vsocket" {
+  tenancy              = "default"
+  ami                  = data.aws_ami.vsocket.id
+  key_name             = var.key_pair
+  instance_type        = var.instance_type
+  user_data            = base64encode(local.primary_serial[0])
+  iam_instance_profile = aws_iam_instance_profile.cato_ha_instance_profile.name
+  # Network Interfaces
+  # MGMTENI
+  network_interface {
+    device_index         = 0
+    network_interface_id = var.mgmt_eni_id
+  }
+  # WANENI
+  network_interface {
+    device_index         = 1
+    network_interface_id = var.wan_eni_id
+  }
+  # LANENI
+  network_interface {
+    device_index         = 2
+    network_interface_id = var.lan_eni_id
+  }
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = var.ebs_disk_size
+    volume_type = var.ebs_disk_type
+  }
+  tags = merge(var.tags, {
+    Name = "${var.site_name}-vSocket"
+  })
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+  depends_on = [aws_route_table_association.lan_secondary_subnet_association]
+}
+
+resource "aws_eip" "mgmt_eip" {
+  domain            = "vpc"
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+}
+
+resource "aws_eip" "wan_eip" {
+  domain            = "vpc"
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+}
+
+# Elastic IP Addresses Association - Required to properly destroy 
+resource "aws_eip_association" "primary_mgmt_eip_assoc" {
+  network_interface_id = var.mgmt_eni_id
+  allocation_id        = aws_eip.wan_eip.id
+}
+
+resource "aws_eip_association" "primary_wan_eip_assoc" {
+  network_interface_id = var.wan_eni_id
+  allocation_id        = aws_eip.mgmt_eip.id
+}
+
+# Route tables for MGMT, WAN and both LAN subnets
+resource "aws_route_table" "route_table" {
+  vpc_id = var.vpc_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table" "route_table_lan" {
+  vpc_id = var.vpc_id
+  route {
+    cidr_block           = "0.0.0.0/0"
+    network_interface_id = var.lan_eni_id
+  }
+}
+
+# Route table associations
+resource "aws_route_table_association" "wan_subnet_association" {
+  subnet_id      = var.wan_subnet_id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "mgmt_subnet_association" {
+  subnet_id      = var.mgmt_subnet_id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "lan_subnet_association" {
+  subnet_id      = var.lan_subnet_id
+  route_table_id = aws_route_table.route_table_lan.id
+}
+
+resource "aws_route_table_association" "lan_secondary_subnet_association" {
+  subnet_id      = var.lan_secondary_subnet_id
+  route_table_id = aws_route_table.route_table_lan.id
+}
+
+# To allow socket to upgrade so secondary socket can be added
+resource "null_resource" "sleep_300_seconds" {
+  provisioner "local-exec" {
+    command = "sleep 300"
+  }
+  depends_on = [ aws_instance.primary_vsocket ]
+}
+
+#################################################################################
+# Add secondary socket to site via API until socket_site resrouce is updated to natively support
+resource "null_resource" "configure_secondary_aws_vsocket" {
+  depends_on = [null_resource.sleep_300_seconds]
+
+  provisioner "local-exec" {
+    command = <<EOF
+      # Execute the GraphQL mutation to get the site id
+      response=$(curl -k -X POST \
+        -H "Accept: application/json" \
+        -H "Content-Type: application/json" \
+        -H "x-API-Key: ${var.token}" \
+        "${var.baseurl}" \
+        --data '{
+          "query": "mutation siteAddSecondaryAwsVSocket($accountId: ID!, $addSecondaryAwsVSocketInput: AddSecondaryAwsVSocketInput!) { site(accountId: $accountId) { addSecondaryAwsVSocket(input: $addSecondaryAwsVSocketInput) { id } } }",
+          "variables": {
+            "accountId": "${var.account_id}",
+            "addSecondaryAwsVSocketInput": {
+              "eniIpAddress": "${var.lan_secondary_local_ip}",
+              "eniIpSubnet": "${var.secondary_native_network_range}",
+               "routeTableId": "${aws_route_table.route_table_lan.id}",
+              "site": {
+                "by": "ID",
+                "input": "${cato_socket_site.aws-site.id}"
+              }
+            }
+          },
+          "operationName": "siteAddSecondaryAwsVSocket"
+        }' )
+    EOF
+  }
+
+  triggers = {
+    account_id = var.account_id
+    site_id    = cato_socket_site.aws-site.id
+  }
+}
+
+
+# Retrieve Secondary vSocket Virtual Machine serial
+data "cato_accountSnapshotSite" "aws-site-secondary" {
+  depends_on = [ null_resource.configure_secondary_aws_vsocket ]
+  id = cato_socket_site.aws-site.id
+}
+
+locals {
+  primary_serial = [for s in data.cato_accountSnapshotSite.aws-site.info.sockets : s.serial if s.is_primary == true]
+}
+
+# Sleep to allow Secondary vSocket serial retrieval
+resource "null_resource" "sleep_10_seconds" {
+  provisioner "local-exec" {
+    command = "sleep 10"
+  }
+  depends_on = [ data.cato_accountSnapshotSite.aws-site-secondary ]
+}
+
+locals {
+  secondary_serial = [for s in data.cato_accountSnapshotSite.aws-site-secondary.info.sockets : s.serial if s.is_primary == false]
+  depends_on = [null_resource.configure_secondary_aws_vsocket]
+}
+
+## vSocket Instance
+resource "aws_instance" "vSocket_Secondary" {
+  tenancy              = "default"
+  ami                  = data.aws_ami.vsocket.id
+  key_name             = var.key_pair
+  instance_type        = var.instance_type
+  user_data            = base64encode(local.secondary_serial[0])
+  iam_instance_profile = aws_iam_instance_profile.cato_ha_instance_profile.name
+  # Network Interfaces
+  # MGMTENI
+  network_interface {
+    device_index         = 0
+    network_interface_id = var.secondary_mgmt_eni_id
+  }
+  # WANENI
+  network_interface {
+    device_index         = 1
+    network_interface_id = var.secondary_wan_eni_id
+  }
+  # LANENI
+  network_interface {
+    device_index         = 2
+    network_interface_id = var.secondary_lan_eni_id
+  }
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = var.ebs_disk_size
+    volume_type = var.ebs_disk_type
+  }
+  tags = merge(var.tags, {
+    Name = "${var.site_name}-vSocket-Secondary"
+  })
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+  depends_on = [null_resource.sleep_10_seconds]
+}
+
+resource "aws_eip" "secondary_mgmt_eip" {
+  domain            = "vpc"
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+}
+
+resource "aws_eip" "secondary_wan_eip" {
+  domain            = "vpc"
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+}
+
+# Elastic IP Addresses Association - Required to properly destroy 
+resource "aws_eip_association" "secondary_mgmt_eip_assoc" {
+  network_interface_id = var.secondary_mgmt_eni_id
+  allocation_id        = aws_eip.secondary_wan_eip.id
+}
+
+resource "aws_eip_association" "secondary_wan_eip_assoc" {
+  network_interface_id = var.secondary_wan_eni_id
+  allocation_id        = aws_eip.secondary_mgmt_eip.id
+}
---
+++ variables.tf
@@ -0,0 +1,174 @@
+## Cato provider variables
+variable "baseurl" {
+  description = "Cato API base URL"
+  type        = string
+  default     = "https://api.catonetworks.com/api/v1/graphql2"
+}
+
+variable "token" {
+  description = "Cato API token"
+  type        = string
+}
+
+variable "account_id" {
+  description = "Cato account ID"
+  type        = number
+}
+
+## vSocket Module Varibables
+variable "site_description" {
+  type        = string
+  description = "Site description"
+}
+
+variable "site_name" {
+  type        = string
+  description = "Your Cato Site Name Here"
+}
+
+variable "native_network_range" {
+  type        = string
+  description = <<EOT
+  	Choose the unique network range your vpc is configured with for your socket that does not conflict with the rest of your Wide Area Network.
+    The accepted input format is Standard CIDR Notation, e.g. X.X.X.X/X
+	EOT
+}
+
+variable "secondary_native_network_range" {
+  type        = string
+  description = <<EOT
+  	Choose the unique network range your vpc is configured with for your socket that does not conflict with the rest of your Wide Area Network.
+    The accepted input format is Standard CIDR Notation, e.g. X.X.X.X/X
+	EOT
+}
+
+variable "site_type" {
+  description = "The type of the site"
+  type        = string
+  validation {
+    condition     = contains(["DATACENTER", "BRANCH", "CLOUD_DC", "HEADQUARTERS"], var.site_type)
+    error_message = "The site_type variable must be one of 'DATACENTER','BRANCH','CLOUD_DC','HEADQUARTERS'."
+  }
+  default = "CLOUD_DC"
+}
+
+variable "site_location" {
+  description = "The location of the site, used for timezone and geolocation.  Use the Cato CLI to get the list of locations. []()"
+  type = object({
+    city         = string
+    country_code = string
+    state_code   = string
+    timezone     = string
+  })
+}
+
+## Virtual Socket Variables
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "connection_type" {
+  description = "Model of Cato vsocket"
+  type        = string
+  default     = "SOCKET_AWS1500"
+}
+
+variable "ebs_disk_size" {
+  description = "Size of disk"
+  type        = number
+  default     = 32
+}
+
+variable "ebs_disk_type" {
+  description = "Size of disk"
+  type        = string
+  default     = "gp2"
+}
+
+variable "instance_type" {
+  description = "The instance type of the vSocket"
+  type        = string
+  default     = "c5.xlarge"
+  validation {
+    condition     = contains(["d2.xlarge", "c3.xlarge", "t3.large", "t3.xlarge", "c4.xlarge", "c5.xlarge", "c5d.xlarge", "c5n.xlarge"], var.instance_type)
+    error_message = "The instance_type variable must be one of 'd2.xlarge','c3.xlarge','t3.large','t3.xlarge','c4.xlarge','c5.xlarge','c5d.xlarge','c5n.xlarge'."
+  }
+}
+
+variable "key_pair" {
+  description = "Name of an existing Key Pair for AWS encryption"
+  type        = string
+  default     = "your-key-pair-name-here"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "mgmt_eni_id" {
+  description = "Managent Elastic Network Interface ID, network interface connected public to a subnet with routable access to the internet to access the internet and the Cato SASE cloud platform. Example: eni-abcde12345abcde12345"
+  type        = string
+}
+
+variable "wan_eni_id" {
+  description = "WAN Elastic Network Interface ID, network interface connected to a public subnet with routable access to the internet to access the internet and the Cato SASE cloud platform. Example: eni-abcde12345abcde12345"
+  type        = string
+}
+
+variable "lan_eni_id" {
+  description = "LAN Elastic Network Interface ID, network interface connected to a private subnet for local VPC resources to connect to for access to internet and WAN access through the Cato socket. Example: eni-abcde12345abcde12345"
+  type        = string
+}
+
+variable "secondary_mgmt_eni_id" {
+  description = "Managent Elastic Network Interface ID, network interface connected public to a subnet with routable access to the internet to access the internet and the Cato SASE cloud platform. Example: eni-abcde12345abcde12345"
+  type        = string
+}
+
+variable "secondary_wan_eni_id" {
+  description = "WAN Elastic Network Interface ID, network interface connected to a public subnet with routable access to the internet to access the internet and the Cato SASE cloud platform. Example: eni-abcde12345abcde12345"
+  type        = string
+}
+
+variable "secondary_lan_eni_id" {
+  description = "LAN Elastic Network Interface ID, network interface connected to a private subnet for local VPC resources to connect to for access to internet and WAN access through the Cato socket. Example: eni-abcde12345abcde12345"
+  type        = string
+}
+
+variable "lan_local_ip" {
+  description = "Choose an IP Address within the LAN Subnet. You CANNOT use the first four assignable IP addresses within the subnet as it's reserved for the AWS virtual router interface used as the default route for private resources to gain access to WAN and internet. The accepted input format is X.X.X.X"
+  type        = string
+}
+
+variable "lan_secondary_local_ip" {
+  description = "Choose an IP Address within the Secodnary LAN Subnet. You CANNOT use the first four assignable IP addresses within the subnet as it's reserved for the AWS virtual router interface used as the default route for private resources to gain access to WAN and internet. The accepted input format is X.X.X.X"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to be appended to AWS resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "wan_subnet_id" {
+  description = "Wan Subnet ID"
+  type        = string
+}
+
+variable "mgmt_subnet_id" {
+  description = "Mgmt Subnet ID"
+  type        = string
+}
+
+variable "lan_subnet_id" {
+  description = "Lan Subnet ID"
+  type        = string
+}
+
+variable "lan_secondary_subnet_id" {
+  description = "lan secondary Subnet ID"
+  type        = string
+}
